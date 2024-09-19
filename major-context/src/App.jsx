928e8b889a112289593cd5efacef5629f67e85c2
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-19 22:35:27 +0530

    Add layout of Components

--- major-context/src/App.jsx
+++ major-context/src/App.jsx
@@ -5,6 +5,12 @@ const App = () => {
   const [themeMode, setThemeMode] = useState("light");
   const lightTheme = () => setThemeMode("light");
   const darkTheme = () => setThemeMode("dark");
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
