3db40b20290ddaa060364bdb8187d4583c53af19
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-19 22:49:09 +0530

    Implement the functionality of theme switching

--- major-context/src/App.jsx
+++ major-context/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 const App = () => {
   const [themeMode, setThemeMode] = useState("light");
@@ -16,10 +18,12 @@ const App = () => {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* Theme Button */}
+            <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
