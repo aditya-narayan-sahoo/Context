1362812c13e666f21beed5b98d5f678ba6061db9
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-19 22:34:16 +0530

    Add Context

--- major-context/src/App.jsx
+++ major-context/src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { ThemeProvider } from "./contexts/theme";
+
 const App = () => {
-  return <div></div>;
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme Button */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
